5a492cfb68e868526c5f584cd0925eefe757f58d
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2021-07-06 17:41:41 +0600

    some code

--- src/RYessaliyev/Rustem.php
+++ src/RYessaliyev/Rustem.php
@@ -4,5 +4,15 @@ namespace RYessaliyev;
 
 class Rustem
 {
+    private string $text;
 
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
 }
